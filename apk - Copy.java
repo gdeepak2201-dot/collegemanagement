31f0144a31282ee5d32bf51a6a6c270f8ceaeddc
DEEPAK G <gdeepak2201@gmail.com>
2026-05-02 11:20:41 +0530

    Fix formatting and import statement in apk - Copy.java

--- apk - Copy.java
+++ apk - Copy.java
@@ -5,6 +5,6 @@ void main() {
 
     collegeapp app = new collegeapp();
     app.addstudent(1,"deepak","8089086294");
-    app.displayallstudent();
+    app.displayallstudent(); 
     app.search();
 }
