6355aa64ed7ac2e01b6405682062348ddc5baf8e
DEEPAK G <gdeepak2201@gmail.com>
2026-05-07 10:47:12 +0530

    Fix method call for displayallstudent

--- apk.java
+++ apk.java
@@ -3,6 +3,6 @@ import college.collegeapp;
 void main() {
     collegeapp app = new collegeapp();
     app.addstudent(1,"deepak","8089086294");
-    app.displayallstudent;
+    app.displayallstudent();
     app.search();  
 } 
